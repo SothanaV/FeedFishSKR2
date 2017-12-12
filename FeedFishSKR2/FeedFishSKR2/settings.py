ca4df2b85f4b3ce86194a04c4d81fff0670b3a92
SothanaV <nai6320@gmail.com>
2017-12-12 17:03:18 +0700

    addLogin

--- FeedFishSKR2/FeedFishSKR2/settings.py
+++ FeedFishSKR2/FeedFishSKR2/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'widget_tweaks',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+LOGIN_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
